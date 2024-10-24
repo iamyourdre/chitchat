ff1d765e867784e93168039a8ca9766759a6b67c
iamyourdre <iamyourdre@gmail.com>
2024-10-24 13:39:59 +0700

    feat: chatController (be)

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,13 +1,12 @@
 import asyncHandler from 'express-async-handler';
 import { Op, Sequelize } from 'sequelize';
-import { ChatRoom, ChatRoomMember } from '../models/chatModel.js';
+import { Chat, ChatRoom, ChatRoomMember } from '../models/chatModel.js';
 import User from '../models/userModel.js';
 
-export const createOneOnOneChat = asyncHandler(async (req, res) => {
+export const createOneOnOneRoom = asyncHandler(async (req, res) => {
   try {
     const { user1_id, user2_number } = req.body;
 
-    // Cek apakah user2 ada di database
     const user2_data = await User.findOne({
       where: {
         phone_number: user2_number
@@ -18,7 +17,6 @@ export const createOneOnOneChat = asyncHandler(async (req, res) => {
       return res.status(404).json({ message: "This user doesn't have ChitChat account" });
     }
 
-    // Cari apakah sudah ada ChatRoom yang berisi kedua user (user1 dan user2)
     const existingChatRoom = await ChatRoom.findOne({
       where: {
         is_group: false
@@ -29,7 +27,15 @@ export const createOneOnOneChat = asyncHandler(async (req, res) => {
           as: 'members',
           where: {
             user_id: { [Op.in]: [user1_id, user2_data.id] }
-          }
+          },
+          attributes: { exclude: ['user_id'] },
+          include: [
+            {
+              model: User,
+              as: 'user_data',
+              attributes: { exclude: ['id', 'password'] }
+            }
+          ],
         }
       ],
       group: ['chat_room.id'],
@@ -54,13 +60,119 @@ export const createOneOnOneChat = asyncHandler(async (req, res) => {
       { chat_room_id: newChatRoom.id, user_id: user2_data.id }
     ]);
 
-    res.status(201).json({
-      chatRoom: newChatRoom,
-      message: 'New chat room created and members added'
-    });
+    res.status(201).json(newChatRoom);
 
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+export const findRoomById = asyncHandler(async (req, res) => {
+  try {
+    const { room_id } = req.body;
+
+    const chatRoom = await ChatRoom.findOne({
+      where: {
+        id: room_id
+      },
+      include: [
+        {
+          model: ChatRoomMember,
+          as: 'members',
+          attributes: { exclude: ['user_id'] },
+          include: [
+            {
+              model: User,
+              as: 'user_data',
+              attributes: { exclude: ['id', 'password'] },
+            }
+          ],
+        },
+      ],
+    });
+
+    res.status(200).json(chatRoom);
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const listRoomByUser = asyncHandler(async (req, res) => {
+  try {
+    const { user_id } = req.body;
+
+    const chatRoom = await ChatRoomMember.findAll({
+      where: {
+        user_id: user_id
+      },
+      include: [
+        {
+          model: ChatRoom,
+          as: 'chat_room',
+          include: [
+            {
+              model: Chat,
+              as: 'last_chat_data'
+            }
+          ],
+        },
+      ],
+    });
+
+    res.status(200).json(chatRoom);
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+export const sendChat = asyncHandler(async (req, res) => {
+  try {
+    const { text, media, sender_id, chat_room_id } = req.body;
+
+    const chat = await Chat.create({
+      text: text,
+      media: media || null,
+      sender_id: sender_id,
+      chat_room_id: chat_room_id
+    });
+    
+    const chatRoom = await ChatRoom.update(
+      {
+        last_chat_id: chat.id
+      },
+      {
+        where: {
+          id: chat_room_id
+        }
+      }
+    );    
+
+    res.status(200).json(chat);
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const refreshChat = asyncHandler(async (req, res) => {
+  try {
+    const { chat_room_id, last_update } = req.body;
+
+    const chat = await Chat.findAll({
+      where: {
+        chat_room_id: chat_room_id,
+        createdAt: {
+          [Sequelize.Op.gt]: new Date(last_update)
+        }
+      }
+    });
+
+    res.status(200).json(chat);
+
+  } catch (error) {
+    console.log(error);
   }
 });
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { protect } from '../middlewares/authMiddleware.js';
-import { createOneOnOneChat } from '../controllers/chatController.js';
+import { createOneOnOneRoom, findRoomById, listRoomByUser, refreshChat, sendChat } from '../controllers/chatController.js';
 
 const router = express.Router();
 
-router.post('/createRoom', protect, createOneOnOneChat)
+router.post('/createRoom', protect, createOneOnOneRoom)
+router.post('/findRoom', protect, findRoomById)
+router.post('/listRoom', protect, listRoomByUser)
+router.post('/send', protect, sendChat)
+router.post('/refresh', protect, refreshChat)
 
 export default router;
